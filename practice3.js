77d9afb3142a8f1bddd8dc9ff3572d6613906a4d
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-26 21:48:35 +0600

    practice4 file add

---
+++ practice3.js
@@ -0,0 +1,4 @@
+var part1 = 'I am going to be';
+var part2 = 'an awesome web developer';
+var line1 = part1 + ' ' + part2;
+console.log(line1);
---
+++ practice4.js
@@ -0,0 +1,4 @@
+var num1 = 119;
+var num2 = 5;
+var output = 119 % 5;
+console.log(output);
