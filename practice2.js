772bc68c22735e6ebde6472da2d2ba1108d76b93
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-26 21:36:22 +0600

    practice2 file add

---
+++ practice2.js
@@ -0,0 +1,10 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalMark = mathematics + biology + chemistry + physics + bangla;
+var averageMarks = totalMark / 5;
+var averageMarks = averageMarks.toFixed(2);
+console.log(averageMarks);
